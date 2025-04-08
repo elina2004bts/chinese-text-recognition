2580f095276dc194b19050b59e550de3e2086469
elina2004bts <elina0701bts@mail.ru>
2025-04-08 13:05:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def load_image():
         return None
 
 
-st.title('Распознай японский текст с изображения!')
+st.title('Распознай китайский текст с изображения!')
 img = load_image()
 
 result = st.button('Распознать изображение')
