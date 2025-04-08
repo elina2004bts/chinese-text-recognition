5e0004ea7c7bfbe2d6846b5c4dfba5dc45311886
elina2004bts <elina0701bts@mail.ru>
2025-04-08 15:25:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 import streamlit as st
 from paddleocr import PaddleOCR
+import os
 
 # Инициализация PaddleOCR с параметрами для китайского языка
 ocr = PaddleOCR(use_angle_cls=True, lang='ch')
 
 st.title("Китайский текст - Распознавание с помощью PaddleOCR")
 
-# Указание статического пути к изображению
-img_path = "C:\\Users\\USer\\Desktop\\китайский.jpg"  # Путь к изображению на вашем компьютере
+# Загрузка изображения через интерфейс Streamlit
+img_file = st.file_uploader("Загрузите изображение с китайским текстом", type=["png", "jpg", "jpeg", "webp"])
 
-# Проверка, существует ли файл по указанному пути
-import os
-if os.path.exists(img_path):
-    st.write(f"Изображение найдено: {img_path}")
-else:
-    st.write(f"Изображение не найдено по пути: {img_path}")
+if img_file is not None:
+    # Считывание изображения
+    img_path = f"./{img_file.name}"
+    with open(img_path, "wb") as f:
+        f.write(img_file.getbuffer())
 
-# Распознавание текста
-result = ocr.ocr(img_path, cls=True)
+    # Проверка, существует ли файл по указанному пути
+    if os.path.exists(img_path):
+        st.write(f"Изображение найдено: {img_path}")
+    else:
+        st.write(f"Изображение не найдено по пути: {img_path}")
 
-# Показ результатов
-st.image(img_path, caption="Загруженное изображение", use_column_width=True)
-st.write("Распознанный текст:")
-for line in result[0]:
-    st.write(line[1])
+    # Распознавание текста
+    result = ocr.ocr(img_path, cls=True)
+
+    # Показ результатов
+    st.image(img_path, caption="Загруженное изображение", use_column_width=True)
+    st.write("Распознанный текст:")
+    for line in result[0]:
+        st.write(line[1])
+
